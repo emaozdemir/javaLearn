8eeecf0cac98f9f109911dc30c81dbf9b1e51fe0
emaozdemir <semaozdemiraslan@gmail.com>
2024-02-22 19:51:01 +0100

    22.02

--- src/j11_Arrays/Tasks/Task05.java
+++ src/j11_Arrays/Tasks/Task05.java
@@ -1,10 +1,28 @@
 package j11_Arrays.Tasks;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task05 {
 
 
     public static void main(String[] args) {
         // Task-> girilen int değeri tersten print eden code create ediniz.
+        Scanner input = new Scanner(System.in);
+        System.out.print("lütfen sayi girin= ");
+        int sayi = input.nextInt();
+        System.out.println(sayi);
+        String str = Integer.toString(sayi);//stringe bu şekilde cevirebilirim.
+        System.out.println(str);
+        // String[] arr = Integer.toString(sayi).split("");//2.yol stringe cevirmek için aynı zamanda split ile bölmüş oldum ve arraye atadım.
+        //System.out.println(Arrays.toString(arr));
+        String[] arr = str.split("");
+        System.out.println(Arrays.toString(arr));
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+
+        }
+
 
     }
 
--- src/j11_Arrays/Tasks/Task08.java
+++ src/j11_Arrays/Tasks/Task08.java
@@ -1,8 +1,28 @@
 package j11_Arrays.Tasks;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task08 {
     public static void main(String[] args) {
         //Task-> girilen 7 elemanlı bir int  arrayın tek elemanlarını print eden code create ediniz.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kaç elemanlı bir array istersin: ");
+        int arrayLength = scanner.nextInt();
+        int[] arr = new int[arrayLength];
+        System.out.println("lutfen istediğiniz elemanları giriniz: ");
+        for (int i = 0; i < arrayLength; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < arrayLength; i++) {
+            if (arr[i]%2!=0) {
+                System.out.print(arr[i]+" ");
+            }
+        }
+
+
 
     }
 }
