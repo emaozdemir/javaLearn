b66e7cade43ee38bd7625c41c151a5cd0fc01ae6
emaozdemir <semaozdemiraslan@gmail.com>
2024-02-26 21:20:55 +0100

    26.02 data-time

---
+++ src/j15_DataTime/C06_Duration.java
@@ -0,0 +1,19 @@
+package j15_DataTime;
+
+import com.sun.tools.javac.Main;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+public class C06_Duration {
+    public static void main(String[] args) {
+        LocalTime gece=LocalTime.of(00,00);
+        LocalTime gunduz=LocalTime.of(12,30);
+        Duration fark1=Duration.between(gece,gunduz);
+        Duration fark2=Duration.between(gunduz,gece);
+        System.out.println("fark1 = " + fark1);//PT12H30M
+        System.out.println("fark2 = " + fark2);//PT-12H-30M
+        System.out.println("fark2.getSeconds() = " + fark2.getSeconds());//-45000
+
+    }
+}
---
+++ src/j15_DataTime/C08_Period.java
@@ -0,0 +1,7 @@
+package j15_DataTime;
+
+public class C08_Period {
+    public static void main(String[] args) {
+        // priod-> iki tarih arasındaki geçen tekrarlana datayı tutar..
+    }
+}
