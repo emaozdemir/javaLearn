d1c7ed5ea825f2f60ffc3260e82b87ac3865bf7d
emaozdemir <semaozdemiraslan@gmail.com>
2024-03-26 21:35:56 +0100

    26.03

---
+++ src/j30_Map/tasks/Find_numbers_of_words.java
@@ -0,0 +1,44 @@
+package j30_Map.tasks;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Find_numbers_of_words {
+
+    ///			KELİME ANALİZİ
+
+    /*
+     * Girilen bir metinde kelimelerin kaç kere geçtiğini bulan program yazınız.
+     *
+     * 1. Adım : ( Kelime = Kaç Kere Geçiyor ) Şeklinde veri tutmak için HashMap oluşturun.
+     * 2. Adım : Girilen metni split methodu ile parçalayalım.
+     * 			 ilk önce "." ya göre parçalayıp cümleleri elde edelim
+     * 			 daha sonra " " boşluğa göre parçalayıp kelimeleri elde edelim
+     * 3. Adım : Tüm kelimeleri kontrol etmek için for döngüsü kullanalım ve tüm kelimeleri oluşturduğumuz hashMap'e aktaralım.
+     * 			 Eğer hashmap'te yoksa ekleyelim ve sayısına 1 yazalım.
+     * 		 	 Eğer hashmap'te zaten varsa, sayısını bir artıralım.
+     * 3. Adım : Tüm kelimeler kontrol edildikten çıktı verelim. Ve tüm kelimeleri tekrar sayısıyla birlikte gösterelim.
+     *
+     * İpucu : "." özel karakter olduğu için "\\." şeklinde split ediniz.
+     * */
+
+
+    ///// örnek bir parçalama çiz
+
+    //		cümle bir. cümle iki
+    //		1. cümle bir --- >> cümle (1) , bir (1)
+    //		2. cümle iki --- >> cümle (2) , iki (1)
+
+
+    //// adım adım süre ver bu soruda
+    //// noktaya göre parçalama 2 dk
+    //// kelimelere göre parçalama 4 dk
+
+    static HashMap<String,Integer> kelimeListesi = new HashMap<>();
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
