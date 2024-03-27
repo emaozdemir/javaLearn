9aa0046f16f6a3cc346a563c0b0cfb424fd2b213
emaozdemir <semaozdemiraslan@gmail.com>
2024-03-27 20:38:11 +0100

    27.03

--- src/j31_Debugging/C01_Debugger.java
+++ src/j31_Debugging/C01_Debugger.java
@@ -2,18 +2,18 @@ package j31_Debugging;
 
 public class C01_Debugger {
     /*
-     Debug yapiyorsan, kodu anlamadin demektir.
-     Debug yaparken kodu anlamiyorsan, nasil debug yapilir, onu anlamadin demektir.
-     Debug yaparken kodu anladigini dusunuyorsan, aslinda debuga ihtiyacin olmadigini anladin demektir.
-     Debug etmek kodun mental bir modelinin eksikligine isarettir...
+ Debug yapiyorsan, kodu anlamadin demektir.
+ Debug yaparken kodu anlamiyorsan, nasil debug yapilir, onu anlamadin demektir.
+ Debug yaparken kodu anladigini dusunuyorsan, aslinda debuga ihtiyacin olmadigini anladin demektir.
+ Debug etmek kodun mental bir modelinin eksikligine isarettir...
 
 
-    İki durumda debugger gerekli olabilir:
-    *  Mevcut kod devralındığında.
-    *  Yeni kod yazıldığında.
-    her iki durumda da debugger çalıştırmamaya dikkat edilmeli.
-    Amac sıfır debug aktivitesi ile uygulamayı istenilen seviyeye getirebilmek.
-*/
+ İki durumda debugger gerekli olabilir:
+ *  Mevcut kod devralındığında.
+ *  Yeni kod yazıldığında.
+ her iki durumda da debugger çalıştırmamaya dikkat edilmeli.
+ Amac sıfır debug aktivitesi ile uygulamayı istenilen seviyeye getirebilmek.
+ */
     static int falanFilan=7;//gokteki gunes her method erisebilir
     public static void main(String[] args) {
         method01();//ev temizligi
